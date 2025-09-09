4b13d0cc292ebbe231af40219a6d082d1b5a4cb1
Nihar Patel <niharpatel52@gmail.com>
2025-09-09 15:44:09 -0400

    Add scope to Strategy model

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -1,4 +1,8 @@
 class Strategy < ApplicationRecord
   has_many :user_strategies
   has_many :users, through: :user_strategies
+
+  scope :low_risk, -> { where(risk_level: 'Low') }
+  scope :medium_risk, -> { where(risk_level: 'Medium') }
+  scope :high_risk, -> { where(risk_level: 'Migh') }
 end
