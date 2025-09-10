0601d53bb4912427ca2774b291f40200cd0d317b
Nihar Patel <niharpatel52@gmail.com>
2025-09-10 10:37:04 -0400

    Create user_strategy_spec to not allow duplicate associations

--- app/models/user_strategy.rb
+++ app/models/user_strategy.rb
@@ -1,4 +1,6 @@
 class UserStrategy < ApplicationRecord
   belongs_to :user
   belongs_to :strategy
+  
+  validates :strategy_id, uniqueness: { scope: :user_id }
 end
--- spec/models/user_strategy_spec.rb
+++ spec/models/user_strategy_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe UserStrategy, type: :model do
   it 'has a valid user_strategy' do
     expect(build(:user_strategy)).to be_valid
   end
+
+  it 'does not allow a duplicate user-strategy' do
+    a = create(:user_strategy)
+    b = build(:user_strategy, user: a.user, strategy: a.strategy)
+    expect(b).not_to be_valid
+  end
 end
