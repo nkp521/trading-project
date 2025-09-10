899fa3245c059817693d9219b9f8581f5ec9715c
Nihar Patel <niharpatel52@gmail.com>
2025-09-10 15:46:22 -0400

    Fix rubocop issue in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,28 @@ class HomeController < ApplicationController
     @form_type = params[:form] || "login"
     @user = User.new
     @login_user = User.new
-    
-    # Restore signup form data and errors from session if present
-    if session[:signup_errors].present?
-      @user.assign_attributes(session[:signup_params] || {})
-      session[:signup_errors].each { |msg| @user.errors.add(:base, msg) }
-      session.delete(:signup_errors)
-      session.delete(:signup_params)
-    end
-    
-    # Restore login form data and errors from session if present
-    if session[:login_errors].present?
-      @login_user.assign_attributes(session[:login_params] || {})
-      session[:login_errors].each { |msg| @login_user.errors.add(:base, msg) }
-      session.delete(:login_errors)
-      session.delete(:login_params)
-    end
+
+    restore_signup_errors
+    restore_login_errors
+  end
+
+private
+
+  def restore_signup_errors
+    return unless session[:signup_errors].present?
+
+    @user.assign_attributes(session[:signup_params] || {})
+    session[:signup_errors].each { |msg| @user.errors.add(:base, msg) }
+    session.delete(:signup_errors)
+    session.delete(:signup_params)
+  end
+
+  def restore_login_errors
+    return unless session[:login_errors].present?
+
+    @login_user.assign_attributes(session[:login_params] || {})
+    session[:login_errors].each { |msg| @login_user.errors.add(:base, msg) }
+    session.delete(:login_errors)
+    session.delete(:login_params)
   end
 end
