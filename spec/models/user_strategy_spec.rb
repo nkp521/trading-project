0f86beb4774f1f41b903d8f330af5bc8afaee3e7
Nihar Patel <niharpatel52@gmail.com>
2025-09-10 10:39:32 -0400

    Add user_strategy tests for valid user and strategy

--- spec/models/user_strategy_spec.rb
+++ spec/models/user_strategy_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UserStrategy, type: :model do
+  it 'requires a user' do
+    expect(build(:user_strategy, user: nil)).not_to be_valid
+  end
+
+  it 'requires a strategy' do
+    expect(build(:user_strategy, strategy: nil)).not_to be_valid
+  end
+
   it 'has a valid user_strategy' do
     expect(build(:user_strategy)).to be_valid
   end
