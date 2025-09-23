e60a1684c50d144c1e1f570122071aa086915555
Nihar Patel <niharpatel52@gmail.com>
2025-09-23 10:18:55 -0400

    Refactor _error partial to a view_component throughout the app

---
+++ app/components/error_component.rb
@@ -0,0 +1,14 @@
+class ErrorComponent < ViewComponent::Base
+  def initialize(object:)
+    @object = object
+  end
+
+
+  private
+
+  attr_reader :object
+  
+  def errors
+    object.errors.full_messages
+  end
+end
