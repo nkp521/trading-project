585391d8fbe659ebe2adf4aaa7f71d2454905069
Nihar Patel <niharpatel52@gmail.com>
2025-09-24 10:26:59 -0400

    Add error_component_spec.rb tests to check methods

--- spec/components/error_component_spec.rb
+++ spec/components/error_component_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe ErrorComponent, type: :component do
 
     expect(page).to_not have_css('#error_explanation')
   end
+
+  it "returns errors when present" do
+    user = User.new
+    user.errors.add(:email, "must be present")
+    component = ErrorComponent.new(object: user)
+    
+    expect(component.send(:errors)).to eq(["Email must be present"])
+  end
+
+  it "returns no errors when valid" do
+    user = User.new
+    component = ErrorComponent.new(object: user)
+    
+    expect(component.send(:errors)).to eq([])
+  end
 end
