91863d006894b1904b6a3fcf8310732b62fd95cc
Nihar Patel <niharpatel52@gmail.com>
2025-09-26 14:33:19 -0400

    Add innerHTML to js controller

--- app/components/news_component.rb
+++
@@ -1,9 +0,0 @@
-class NewsComponent < ViewComponent::Base
-  def initialize(articles: [])
-    @articles = articles || "No articles"
-  end
-
-  private
-
-  attr_reader :articles
-end
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -7,35 +7,51 @@ export function fetchApiKey() {
     .catch(err => console.error(err));
 }
 
-const fetchNews = (ticker) => {
-  fetchApiKey()
-  .then(apiKey => {
-    const from = "2025-09-01";
-    const to = new Date().toJSON().slice(0, 10);
-    const url = `https://finnhub.io/api/v1/company-news?symbol=${ticker}&from=${from}&to=${to}&token=${apiKey}`;
-
-    fetch(url)
-      .then(res => res.json())
-      .then(data => displayNews(data))
-      .catch(err => console.error(err));
-  }).catch(err => console.error(err));
-};
-
-function displayNews(data) {
-  const top10 = data.slice(0, 10)
-  console.log(top10)
-}
-
 export default class extends Controller {
-  static targets = ["input"]
+  static targets = ["input", "newsContainer"]
 
   search() {
     const ticker = this.inputTarget.value.trim().toUpperCase()
-
     this.load(ticker)
   }
 
   load(ticker) {
-    fetchNews(ticker)
+    this.fetchNews(ticker)
+  }
+
+  fetchNews(ticker) {
+    fetchApiKey()
+    .then(apiKey => {
+      const from = "2025-09-01";
+      const to = new Date().toJSON().slice(0, 10);
+      const url = `https://finnhub.io/api/v1/company-news?symbol=${ticker}&from=${from}&to=${to}&token=${apiKey}`;
+
+      fetch(url)
+        .then(res => res.json())
+        .then(data => this.displayNews(data))
+        .catch(err => console.error(err));
+    }).catch(err => console.error(err));
+  }
+
+  displayNews(data) {
+    const top10 = data.slice(0, 10)
+    
+    let html = '<div>'
+    if (top10.length > 0) {
+      top10.forEach(article => {
+        html += `
+          <div>
+            <h3>${article.headline}</h3>
+            <h5>${article.summary}</h5>
+            <p>Source: ${article.source}</p>
+          </div>
+        `
+      })
+    } else {
+      html += '<p>No articles available</p>'
+    }
+    html += '</div>'
+    
+    this.newsContainerTarget.innerHTML = html
   }
 }
